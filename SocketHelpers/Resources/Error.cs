6ef3497107bf73b7f1c3eb68d71ca51f0045af9a
Ruben Arrebola <ruben.arre6@gmail.com>
2017-11-26 19:50:10 +0100

    Projects added

---
+++ SocketHelpers/Resources/Error.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Mime;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SocketHelpers
+{
+    class ClErrors
+    {
+        public static Boolean blFileError = true;
+        public static Boolean blConsoleError = true;
+        public static Boolean blMsgError = false;
+        public static Boolean blBddError = false;
+        public static Boolean btMailError = false;
+
+        private static String fileError = "";
+
+        public static String FileError
+        {
+            get { return fileError; }
+            set
+            {
+                if (value != "")
+                    fileError = value;
+            }
+        }
+
+        public static void reportError(String errorMsg)
+        {
+            errorMsg = DateTime.Now + ": " + errorMsg;
+            if (blFileError)
+            {
+                if (fileError == "")
+                {
+                    fileError = Application.StartupPath + "/errorLogs.txt";
+                }
+                File.WriteAllText(fileError, errorMsg, Encoding.UTF8);
+            }
+            if (blConsoleError)
+            {
+                Console.WriteLine(errorMsg);
+            }
+            if (blMsgError)
+            {
+                MessageBox.Show(errorMsg, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            if (blBddError)
+            {
+                
+            }
+            if (btMailError)
+            {
+                
+            }
+        }
+    }
+}
---
+++ SocketHelpers/ServerConnection/ClConnection.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Quobject.SocketIoClientDotNet.Client;
+
+namespace SocketHelpers
+{
+    public class ServerConnection
+    {
+        public Quobject.SocketIoClientDotNet.Client.Socket socketServer;
+
+        public event EventHandler gameInfo;
+        public event EventHandler neighborChange;
+        public event EventHandler positionConfirmed;
+        private String _returnedFromServer = "";
+
+
+        public Boolean connectSocketServer(String host)
+        {
+            Boolean done = false;
+
+            if (host != "")
+            {
+                try
+                {
+                    socketServer = IO.Socket(host);
+                    done = true;
+
+                    socketServer.On("gameInfo", (infoPartida) =>
+                    {
+                        _returnedFromServer = infoPartida.ToString();
+                        Console.WriteLine("gameInfo");
+                        gameInfo(_returnedFromServer, EventArgs.Empty);
+                    });
+
+                    socketServer.On("neighborChange", (data) =>
+                    {
+                        _returnedFromServer = data.ToString();
+                        Console.WriteLine("neighborChange");
+                        neighborChange(_returnedFromServer, EventArgs.Empty);
+                    });
+
+                    socketServer.On("positionConfirmed", (data) =>
+                    {
+                        _returnedFromServer = data.ToString();
+                        Console.WriteLine("positionConfirmed");
+                        positionConfirmed(_returnedFromServer, EventArgs.Empty);
+                    });
+
+
+                }
+                catch (Exception e)
+                {
+                    ClErrors.reportError(e.Message);
+                }
+            }
+            else ClErrors.reportError("Can't connect socket is empty!");
+
+            return done;
+        }
+
+        public void disconnectSocketServer()
+        {
+            socketServer.Disconnect();
+        }
+
+        public void selectPosition(String data)
+        {
+            //Datos en forma de Json
+            //data = "{\"pcs\":[{\"Name\":\"Chikorita\",\"Ip\":\"192.168.3.45\"},{\"Name\":\"Ruben\",\"Ip\":\"192.168.3.58\"}],\"cliente\":{\"Name\":\"Abraham\",\"Ip\":\"192.168.3.1\"},\"pos\":2,\"wall\":0}";
+            socketServer.Emit("selectPosition", data);
+        }
+    }
+
+}
